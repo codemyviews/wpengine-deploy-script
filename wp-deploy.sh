e3528287e522dc39c887cd8b248d18d3d04db9c3
Connor Hood <cph@dev>
2019-08-20 14:43:59 +0200

    first commit

---
+++ wp-deploy.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+#
+# Deploy your branch on WPEngine.
+#
+
+set -ex
+
+BRANCH="master"
+SRC_DIR="${$PWD}"
+BUILD_DIR="${SRC_DIR}/.wpengine-deployment-$(date +%s)"
+REPO_SSH_URL="git@git.wpengine.com:production/boltfinancial.git"
+COMMIT_SHA="6c0a8e8133eb1a500bd489b09bc9bcaaa8acb72d"
+DEPLOY_BRANCH="${BRANCH}"
+THEME_NAME="bolt"
+
+cd $SRC_DIR
+COMMIT_AUTHOR_NAME="$( git log --format=%an -n 1 ${COMMIT_SHA} )"
+COMMIT_AUTHOR_EMAIL="$( git log --format=%ae -n 1 ${COMMIT_SHA} )"
+COMMIT_COMMITTER_NAME="$( git log --format=%cn -n 1 ${COMMIT_SHA} )"
+COMMIT_COMMITTER_EMAIL="$( git log --format=%ce -n 1 ${COMMIT_SHA} )"
+
+if [[ -d "$BUILD_DIR" ]]; then
+	echo "ERROR: ${BUILD_DIR} already exists."
+	echo "This should not happen."
+	exit 1
+fi
+
+echo "Deploying ${BRANCH}"
+
+# Making the directory we're going to sync the build into
+git clone "${REPO_SSH_URL}" "${BUILD_DIR}"
+cd $SRC_DIR
+
+
+# Copy the files over
+# -------------------
+
+echo "Syncing files... quietly"
+
+rsync -av --delete --exclude "themes/${THEME_NAME}/.gitignore" "${SRC_DIR}/wp-content/" "${BUILD_DIR}/wp-content/"
+
+
+# Add changed files, delete deleted, etc, etc, you know the drill
+cd ${BUILD_DIR}
+git add -A .
+
+if [ -z "$(git status --porcelain)" ]; then
+	echo "NOTICE: No changes to deploy"
+	exit 0
+fi
+
+# Commit it.
+git commit -a -m "Commit https://bitbucket.org/codemyviews/boltcom/commits/${COMMIT_SHA} at $(date +%s)"
+
+
+# Push it (push it real good).
+git push origin "${DEPLOY_BRANCH}"
